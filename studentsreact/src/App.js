824a2ddaba21c6ee2edf452dd57d771afaca2379
Gabriel Figueiredo <gabrielcarolino962@gmail.com>
2022-04-23 01:28:42 -0300

    ADDED: Modal to include students

--- studentsreact/src/App.js
+++ studentsreact/src/App.js
@@ -3,15 +3,37 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import {Modal, ModalBody, ModalFlooter, ModalHeader} from 'reactstrap';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 import logoRegister from './asset/images.png'
 
 function App() {
   
   const baseUrl = "https://localhost:44300/api/Student";
+
   const [data, setData] = useState([]);
 
-  const getRequest = async() =>{
+  const[IncludeModal, setIncludeModal]=useState(false);
+
+  const [selectedStudent, setSelectedStudent]=useState({
+    id: '',
+    name:'',
+    email:'',
+    age:''
+  })
+
+  const openCloseIncludeModal=()=>{
+    setIncludeModal(!IncludeModal);
+  }
+
+  const handleChange = e=>{
+    const {name, value} = e.target;
+    setSelectedStudent({
+      ...selectedStudent, [name]:value
+    });
+    console.log(selectedStudent);
+  }
+
+  const getRequest = async()=>{
     await axios.get(baseUrl)
     .then(response => {
       setData(response.data);
@@ -20,7 +42,19 @@ function App() {
     })
   }
 
-  useEffect(() =>{
+  const postRequest = async()=>{
+    delete selectedStudent.id;
+    selectedStudent.age=parseInt(selectedStudent.age);
+      await axios.post(baseUrl, selectedStudent)
+    .then(response=>{
+      setData(data.concat(response.data));
+      openCloseIncludeModal();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  useEffect(()=>{
     getRequest();
   })
 
@@ -30,7 +64,7 @@ function App() {
       <h3>Students Register</h3>
       <header >
         <img src = {logoRegister} alt='Register'/>
-        <button className="btn btn-success"> Include new student</button>
+        <button className="btn btn-success" onClick={()=>openCloseIncludeModal()}> Include new student</button>
       </header>
       <table className="table table-bordered">
         <thead>
@@ -57,6 +91,31 @@ function App() {
           ))}
         </tbody>
       </table>
+
+      <Modal isOpen={IncludeModal}>
+        <ModalHeader>Inclue student</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Name: </label>
+            <br/>
+            <input type="text" className="form-control" name="name" onChange={handleChange}/>
+            <br/>
+            <label>Email: </label>
+            <br/>
+            <input type="text" className="form-control" name="email" onChange={handleChange}/>
+            <br/>
+            <label>Age: </label>
+            <br/>
+            <input type="text" className="form-control" name="age" onChange={handleChange}/>
+            <br/>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={()=>postRequest()}>Include</button>{"  "}
+          <button className="btn btn-danger"  onClick={()=>openCloseIncludeModal()}>Cancel</button>
+        </ModalFooter>
+      </Modal>
+
     </div>
   );
 }
